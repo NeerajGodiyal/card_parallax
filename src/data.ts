4448791c971512c7dd35ab7d77aa9c8385b29df7
Neeraj <neerajgodiyal250@gmail.com>
2024-02-07 09:10:04 +0530

    first commit

---
+++ src/data.ts
@@ -0,0 +1,37 @@
+export const projects = [
+    {
+      title: "Lorem ipsum dolor sit amet",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      src: "rock.jpg",
+      url: "https://github.com/NeerajGodiyal",
+      color: "#BBACAF"
+    },
+    {
+      title: "Lorem ipsum dolor sit amet",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      src: "tree.jpg",
+      url: "https://github.com/NeerajGodiyal",
+      color: "#977F6D"
+    },
+    {
+      title: "Lorem ipsum dolor sit amet",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      src: "water.jpg",
+      url: "https://github.com/NeerajGodiyal",
+      color: "#C2491D"
+    },
+    {
+      title: "Lorem ipsum dolor sit amet",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      src: "house.jpg",
+      url: "https://github.com/NeerajGodiyal",
+      color: "#B62429"
+    },
+    {
+      title: "Lorem ipsum dolor sit amet",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      src: "cactus.jpg",
+      url: "https://github.com/NeerajGodiyal",
+      color: "#88A28D"
+    }
+  ]
